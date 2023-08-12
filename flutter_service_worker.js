ff859e550f67bdf6b97071b40596506c14cb2c22
Bertalan Krisztian <krisztian652@gmail.com>
2023-08-12 09:03:46 +0200

    add additional parameters to clients, add client's attributes to the client task editor page for modification.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,9 +38,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ab6bd404352270ba1a5838070a121503
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3fbc9e30d954d204eefc7c59c57a587d",
-"/": "3fbc9e30d954d204eefc7c59c57a587d",
-"main.dart.js": "f0cafc516c3a6be37b0fba8ce1dcfbb0",
+"index.html": "f9d994b92da393cc84a59c2ed44c249b",
+"/": "f9d994b92da393cc84a59c2ed44c249b",
+"main.dart.js": "8de5e1f6f1e7842d4587d78ddb8374f6",
 "manifest.json": "3781dac72550d6763c3b352a6feb741b",
 "version.json": "3f7c89d5d4fe876440143af813ccc8f2"};
 // The application shell files that are downloaded before a service worker can
